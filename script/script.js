57aae57c6015b7785b94281f4835afb89f63b565
__________ <temurbekshukurov0707@gmail.com>
2022-05-12 09:54:42 +0500

    upload

--- script/script.js
+++ script/script.js
@@ -1,23 +1,29 @@
 $(document).ready(function () {
-    var $page = $('#page').val()
-    var $design = $('#design').val()
-    var $media = $('#media').val()
-    var $seo = $('#seo').val()
-    var $lang = $('#lang').val()
-    var $name = $('#name').val()
-    var $copy = $('#copy').val()
+    var $page = $('input[name = page]')
+    var $design = $('#design')
+    var $media = $('#media')
+    var $seo = $('#seo')
+    var $lang = $('#lang')
+    var $name = $('#name')
+    var $copy = $('#copy')
+    var $but = $('.Btn-button')
 
     $('.Btn-button').on('click', function () {
-        function page() {
-            let pages = $page
-            for (i = 0; i <= pages; i += pages) {
-               pages = pages * 100000
-            }console.log(pages);
+        var a = +$page.val() * 200000;
+        var b = +$design.val();
+        var c = +$media.val();
+        var s = +$seo.val();
+        var l = +$lang.val();
+        var n = +$name.val();
+        var py = +$copy.val();
+        var total = a + b + c + s + l + n + py;
+        $but.disabled = true;
+        $('#display').html(total + ` so'm`);
+        var arr = [a];
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].$('input', function () {
+                $but.disabled = false;
+            })
         }
-        page()
-
-
-
-
     })
 });
